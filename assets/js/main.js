471e07934ab75a1dd3fe42612ba4765c00fe807f
Maria Paz <maria.pazrojasc@gmail.com>
2017-06-06 14:32:28 -0400

    menu gris

---
+++ assets/js/main.js
@@ -0,0 +1,66 @@
+var barra = document.getElementById("barra")
+
+
+barra.addEventListener("click",function(){
+	var lista2 = document.getElementsByClassName("lista2")[0];
+	var uno = document.createElement("div");
+		uno.setAttribute("class","general");
+	var parr = document.createElement("p");
+		parr.setAttribute("class","texto")
+	var home = document.createTextNode("Home");
+
+	var dos = document.createElement("div");
+		dos.setAttribute("class","general");
+	var parr2 = document.createElement("p");
+		parr2.setAttribute("class","texto")
+	var about = document.createTextNode("About");
+
+	var tres = document.createElement("div");
+		tres.setAttribute("class","general");
+	var parr3 = document.createElement("p");
+		parr3.setAttribute("class","texto")
+	var product = document.createTextNode("Product");
+
+	var cuatro = document.createElement("div");
+		cuatro.setAttribute("class","general");
+	var parr4 = document.createElement("p");
+		parr4.setAttribute("class","texto")
+	var serv = document.createTextNode("Services");
+
+	var cinco = document.createElement("div");
+		cinco.setAttribute("class","general");
+	var parr5 = document.createElement("p");
+		parr5.setAttribute("class","texto")
+	var cust = document.createTextNode("Customers");
+
+	var seis = document.createElement("div");
+		seis.setAttribute("class","general");
+	var parr6 = document.createElement("p");
+		parr6.setAttribute("class","texto")
+	var cont = document.createTextNode("Contact");
+
+		parr.appendChild(home);
+		uno.appendChild(parr);
+		lista2.appendChild(uno);
+
+		parr2.appendChild(about);
+		dos.appendChild(parr2);
+		lista2.appendChild(dos);
+
+		parr3.appendChild(product);
+		tres.appendChild(parr3);
+		lista2.appendChild(tres);
+
+		parr4.appendChild(serv);
+		cuatro.appendChild(parr4);
+		lista2.appendChild(cuatro);
+
+		parr5.appendChild(cust);
+		cinco.appendChild(parr5);
+		lista2.appendChild(cinco);
+
+		parr6.appendChild(cont);
+		seis.appendChild(parr6);
+		lista2.appendChild(seis);
+
+})
